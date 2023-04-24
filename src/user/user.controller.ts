531abc8bfca36674b7be070de700b7d02e0a349c
mateus <m4teusmatos@gmail.com>
2023-04-24 02:49:15 -0300

    feat: create DTO using pipe

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,30 @@
-import { Controller, Post, Get, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Body,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserService } from './user.service';
 import { UserEntity } from './entities/user.entity';
+import { returnUserDto } from './dtos/returnUser.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @UsePipes(ValidationPipe)
   @Post()
   async createUser(@Body() createUser: CreateUserDto): Promise<UserEntity> {
     return this.userService.createUser(createUser);
   }
 
   @Get()
-  async getAllUser(): Promise<UserEntity[]> {
-    return this.userService.getAllUser();
+  async getAllUser(): Promise<returnUserDto[]> {
+    return (await this.userService.getAllUser()).map(
+      (userEntity) => new returnUserDto(userEntity),
+    );
   }
 }
