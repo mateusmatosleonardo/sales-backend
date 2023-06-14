ecfbc1770a0c22dddebb42bf619a47e2dcd762c1
mateus <m4teusmatos@gmail.com>
2023-06-14 20:35:21 -0300

    feat: creating createUserMock

---
+++ src/user/__mocks__/createUser.mock.ts
@@ -0,0 +1,9 @@
+import { CreateUserDto } from '../dtos/createUser.dto';
+
+export const createUserMock: CreateUserDto = {
+  name: 'nameMockTest',
+  email: 'emailMockTest@email.com',
+  cpf: '12345678910',
+  password: 'randomPass',
+  phone: '88988888888',
+};
