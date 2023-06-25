cd41f89efd47763014c68e18275466a434d02da6
mateus <m4teusmatos@gmail.com>
2023-06-24 22:42:59 -0300

    remoiving user service app

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,13 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { compare } from 'bcrypt';
-import { UserEntity } from '../user/entities/user.entity';
+
 import { UserService } from '../user/user.service';
 import { LoginDto } from './dtos/login.dto';
 import { ReturnLogin } from './dtos/returnLogin.dto';
 import { ReturnUserDto } from '../user/dtos/returnUser.dto';
 import { LoginPayload } from './dtos/loginPayload.dto';
 import { JwtService } from '@nestjs/jwt';
+import { UserEntity } from '../user/entities/user.entity';
 
 @Injectable()
 export class AuthService {
